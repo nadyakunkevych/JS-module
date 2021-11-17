60c0bcd7098a13666527db109d04082949aec2c2
nadiakunkevic <nadyakunkevych@gmail.com>
2021-11-17 11:33:23 +0200

    update hm

--- classwork_3.js
+++ classwork_3.js
@@ -90,4 +90,21 @@
     // for (let i = twoBack.length-1; i >= 0; i--) {
     // console.log(twoBack[i]);
     // }
+//
+// //
+//     let sixBack = [2,17,13,6,22,31,45,66,100,-18];
+//     for(let i = sixBack.length-1; i >= 0; i--) {
+//         if(i % 2 === 0) {
+//             console.log(sixBack[i]);
+//         }
+//     }
 
+    // let sevenBack = [2,17,13,6,22,31,45,66,100,-18];
+    // for(let i = sevenBack.length-1; i >= 0; i--) {
+    //     if(sevenBack[i] % 3 === 0) {
+    //         sevenBack[i]= "Okten";
+    //         console.log(sevenBack[i]);
+    //     }
+    // }
+    // console.log(sevenBack)
+    //
