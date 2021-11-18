0751faa763367294bd64f5c62b03394da15393b7
nadiakunkevic <nadyakunkevych@gmail.com>
2021-11-18 22:18:52 +0200

    update hm

---
+++ homework_4.js
@@ -0,0 +1,63 @@
+// 1. Створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+//
+//     function s (a, b) {
+//     return a * b;
+//     }
+//     console.log(s(4, 6));  - цей запис вірний? Моя первісна думка була, що так має бути
+//
+// or:
+
+    // function rectangle (a, b) {
+    // let s = a * b;
+    // return s;
+    // }
+    // console.log(rectangle(4, 6))
+
+
+   // 2. Створити функцію яка обчислює та повертає площу кола з радіусом r
+
+    // function s (Pi, r) {
+    //     return Math.PI*Math.pow(r,2);
+    // }
+    // console.log(s(3.14,4));
+
+ // or:
+
+    // function s (Pi, r) {
+    // let circle = Math.PI*Math.pow(r,2);
+    // return circle;
+    // }
+    // console.log(s(3.14, 4))
+    //
+    //
+
+
+
+// 3. Створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+
+    // function  s (r, h) {
+    //     return 2*Math.PI*r*h;
+    // }
+    // console.log(s(4, 6))
+
+// or:
+
+    // function s (r, h) {
+    // let cylinder = 2*Math.PI*r*h;
+    // return cylinder;
+    // }
+    // console.log(s(4, 6))
+
+// 4. Створити функцію яка приймає масив та виводить кожен його елемент
+
+    // let array = ["Ukraine", "Turkey", "France", "Germany"];
+    // function arr (array) {
+    //     for (let i = 0; i < array.length; i++) {
+    //
+    //     }
+    // }
+    // console.log(array[0]);
+    // console.log(array[1]);
+    // console.log(array[2])
+    // console.log(array[3])
+
