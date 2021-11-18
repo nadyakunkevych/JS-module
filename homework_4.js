2faf07681ec9f209cdb08fe7214e7966d1bbcfac
nadiakunkevic <nadyakunkevych@gmail.com>
2021-11-19 00:08:07 +0200

    update hm

--- homework_4.js
+++ homework_4.js
@@ -58,7 +58,7 @@
     // }
     // console.log(array[0]);
     // console.log(array[1]);
-    // console.log(array[2])
+    // console.log(array[2]);
     // console.log(array[3])
 
 // 5. Створити функцію яка створює параграф з текстом. Текст задати через аргумент
@@ -71,26 +71,67 @@
 //     paragraph("Welcome to Okten :)");
 
 
-// 6. створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+// 6. Створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
 //         function ul (days) {
 //         document.write(`<ul>`)
-//             document.write(`<li> ${days} </li>`)
-//             document.write(`<li> ${days} </li>`)
-//             document.write(`<li>${days}</li>`)
+//             document.write(`<li> ${days} </li>`);
+//             document.write(`<li> ${days} </li>`);
+//             document.write(`<li>${days}</li>`);
 // }
-//         document.write(`</ul>`)
+//         document.write(`</ul>`);
 //
 //         ul("Homework")
 //
 //     function ul (days) {
-//         document.write(`<li> ${days} </li>`)
-//             document.write(`<li> ${days} </li>`)
-//             document.write(`<li>${days}</li>`)
+//         document.write(`<li> ${days} </li>`);
+//             document.write(`<li> ${days} </li>`);
+//             document.write(`<li>${days}</li>`);
 //     }
 //
 //         ul("Homework") - а такий запис прийнятний?
 
-// 7. створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-// 8. створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-//
-// 9. створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+// 7. Створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+
+    //
+    // function ul (text, number) {
+    //     document.write(`<ul>`)
+    //     for (let i = 0; i < number; i++) {
+    //         document.write(`<li>${text}</li>`)
+    //     }
+    //     document.write(`</ul>`)
+    //
+    //         }
+    //  ul("classwork", 3)
+    //
+    //
+
+
+// 8. Створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+
+    // let arr = [3, "text", true];
+    // function elements (array) {
+    // document.write(`<ul>`)
+    //     document.write(`<li>${array}</li>`);
+    //     document.write(`<li>${array}</li>`);
+    //     document.write(`<li>${array}</li>`);
+    // }
+    // document.write(`</ul>`)
+    // elements(arr)
+
+// 9. Створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ.
+// Для кожного об'єкту окремий блок.
+
+
+    // let array = [
+    //     {id: 1, name: "Nancy", age: 25 },
+    //     {id: 2, name: "Christopher", age: 20 },
+    //     {id: 3, name: "Tom", age: 35 },
+    //     {id: 4, name: "Dan", age: 40 } ]
+    //
+    // function elements (arr) {
+    //     for (const arrayObject of arr) {
+    //         document.write(`<div>${arrayObject.id}. ${arrayObject.name} - ${arrayObject.age}</div>`)
+    //
+    //     }
+    // }
+    // elements(array)
