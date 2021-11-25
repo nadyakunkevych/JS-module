c60ecff87bbc2a60ad4c8dcb8d605d1b96962cbb
nadiakunkevic <nadyakunkevych@gmail.com>
2021-11-26 00:12:55 +0200

    update hm

--- homework_5_additional.js
+++ homework_5_additional.js
@@ -1,41 +1,26 @@
 // 1. Дано натуральное число n. Выведите все числа от 1 до n.
-
-    // let array = [1, 2, 3, 4, 5, 6, 7, 8];
-
-    //         // for(let i = 0; i < n.length; i++) {
-    //             let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    //             let n = 9;
-    //             for(const elements of array) {
-    //                 if(n <= 9) {
-    //                 }
-    //                     console.log(elements)
-    // }
-    //
-    //
+//
+//             let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+//             for(let i = 0; i < array.length;) {
+//                 console.log(++i)
+//             }
 
 
 // 2. Даны два целых числа A и В . Выведите все числа от A до B включительно, в порядке возрастания, если A < B, или в порядке убывания в противном случае.
 //
 //    let integer = (A, B) => {
-//     for(let i = 0; i < 20; i++) {
-//         if(A < B ) {
-//             console.log(A)
 //
-//         }
-//     }
-//        if(A !== B) {
-//            console.log(A+1, B)
-//     }
-//        else {
-//            console.log(A-1, B)
-//        }
-//     }
+//        for (let i = 0; i < 10;) {
+//            if (A < B) {
+//                console.log(++i);
+//            } else if (B > A) {
+//                console.log(i--);
+//            }
 //
-//    integer(10, 20)
+//                }
+//    }
 //
-
-
-
+// integer(10, 20)
 
 
 
